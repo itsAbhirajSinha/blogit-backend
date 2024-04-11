b50e80ce652150ef4885a24ed4e5db9990588da9
godlovesus <abhirajsinha777@gmail.com>
2024-04-11 14:06:26 +0530

    updating cerify token

--- verifyToken.js
+++ verifyToken.js
@@ -1,22 +1,24 @@
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-const verifyToken=(req,res,next)=>{
-    const token=req.cookies.token
-    // console.log(token)
-    if(!token){
-        return res.status(401).json("You are not authenticated!")
+const verifyToken = async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json("You are not authenticated!");
     }
-    jwt.verify(token,process.env.SECRET,async (err,data)=>{
-        if(err){
-            return res.status(403).json("Token is not valid!")
-        }
-        
-        req.userId=data._id
-       
-        // console.log("passed")
-        
-        next()
-    })
-}
 
-module.exports=verifyToken
+    try {
+        const data = await new Promise((resolve, reject) => {
+            jwt.verify(token, process.env.SECRET, (err, decoded) => {
+                if (err) reject(err);
+                resolve(decoded);
+            });
+        });
+
+        req.userId = data._id;
+        next();
+    } catch (err) {
+        return res.status(403).json("Token is not valid!");
+    }
+};
+
+module.exports = verifyToken;
